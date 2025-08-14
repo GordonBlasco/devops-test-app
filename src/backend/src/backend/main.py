18c762aaad4d5394646edc0230a07a68c29448ea
Gordon Blasco <gordonblasco@gmail.com>
2025-08-13 22:13:30 -0700

    fix(backend): fix gha (#16)

--- src/backend/src/backend/main.py
+++ src/backend/src/backend/main.py
@@ -16,7 +16,7 @@ from .config import (
 )
 
 # trigger a package build
-# fix lower another oh another
+# fix lower another oh another and again
 # Configure logging
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
