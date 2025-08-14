fd6ba559811adbcdb321ac1673bfcf2a63abf566
Gordon Blasco <gordonblasco@gmail.com>
2025-08-13 22:28:14 -0700

    fix(backend): more fixes (#18)

--- src/backend/src/backend/main.py
+++ src/backend/src/backend/main.py
@@ -16,7 +16,7 @@ from .config import (
 )
 
 # trigger a package build
-# fix lower another oh another and again
+# fix lower another
 # Configure logging
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
