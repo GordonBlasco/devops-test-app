5688a70c75160c589781b27c19520a4b41d074d3
Gordon Blasco <gordonblasco@gmail.com>
2025-08-14 19:47:18 -0700

    fix(frontend): fix typo (#24)

--- src/frontend/src/frontend/main.py
+++ src/frontend/src/frontend/main.py
@@ -10,7 +10,7 @@ logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
 logger = logging.getLogger(__name__)
-# trigger ci
+# trigger ci again
 # Create Flask application
 app = Flask(__name__)
 
